8c74535cfc84ef9eb6ebfcc47aa4707e36d8580f
Mirko Bonadei <mirko.bonadei@gmail.com>
2016-11-26 18:46:29 +0100

    Setting up some helm related stuff...

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -13,6 +13,15 @@
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 
+(projectile-mode 1)
+
+(helm-mode 1)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x r b") 'helm-bookmarks)
+
 ;; Backup files
 (setq backup-by-copying t)
 (setq backup-directory-alist '(("" . "~/.emacs-saves")))
